7615698290008fea8423c0355dfb3b433b37e101
dsevero <danielsouzasevero@gmail.com>
2019-10-21 21:44:29 -0300

    Update sha

--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/3778/ml-challenge/master/.sha256sum | sha256sum -c
+curl -s https://raw.githubusercontent.com/3778/ml-challenge/master/.sha256sum | sha256sum -c
